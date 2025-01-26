a43385106e176d69bc922a59c5c8c79e1458abc1
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-01-26 23:15:09 +0530

    Add files via upload

---
+++ number grp.py
@@ -0,0 +1,9 @@
+number = int(input ("enter a number:"))
+if(number>50):
+    print ("the number belongs to first grp")
+if(number>40):
+    if(number<50):
+        print ("the number belongs to second grp")
+else:
+    print("the number belongs to the third grp")
+
