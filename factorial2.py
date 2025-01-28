b585920320f4c1528d2c3dd432dd11d7feaa9eff
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-01-28 09:29:50 +0530

    added fibonnaci

--- factorial2.py
+++ factorial2.py
@@ -1,7 +1,7 @@
 def fact(n):
     if(n==1):
         return 1
-    else:
+    else:                                                                                                       
         return n*fact (n-1)
 n=int(input("enter a number"))
 fact = fact(n)
