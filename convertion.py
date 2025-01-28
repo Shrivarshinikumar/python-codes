f62e0efd254ef71c01dcc1c414b85fb27ff6b64a
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-01-28 14:40:26 +0530

    added conversion

---
+++ convertion.py
@@ -0,0 +1,9 @@
+decimal = int(input("enter a decimal number"))
+
+binary =bin(decimal)
+octal = oct(decimal)
+hexadecimal = hex(decimal)
+
+print("the binary conversion is",binary)
+print("the octal conversion is",octal)
+print("the hexadecimal conversion is",hexadecimal)
