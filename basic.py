f36726510340a7bc2dbdb237a9c4d54dcf56432b
Shrivarshinikumar <shrivarshini.kumar916@gmail.com>
2025-03-11 12:11:38 +0530

    added new program

---
+++ basic.py
@@ -0,0 +1,8 @@
+input1 = 3521
+input2 = 2452
+input3 = 1352
+print(5)
+print(5)
+print(5)
+add= 5+5+5
+print(add)
--- even or odd.py
+++ even or odd.py
@@ -3,9 +3,4 @@ num = int (input("enter the number"))
 if(num%2==0):
     print("the number is even ")
 else:
-
-num = int (input("enter the number"))
-if(num%2==0):
-    print("the number is even ")
-else:
-    print("the number is odd")
+    num= int (input("enter the number"))
